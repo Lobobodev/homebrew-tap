5994c95ca0773a8cca42e9a3c4f293e1f77b70d2
Lobo <mn4h5vyg47@privaterelay.appleid.com>
2026-05-18 00:42:21 +0800

    Update codexbarmenubar to v0.26.1

--- Casks/codexbarmenubar.rb
+++ Casks/codexbarmenubar.rb
@@ -1,6 +1,6 @@
 cask "codexbarmenubar" do
   version "0.26.1"
-  sha256 "9b3e4410ebb9288e22c7b628fa0b70a99ace98c66615849cc07848dcf746b8e0"
+  sha256 "9e954d139527b6ea88c8226e9707dde217a437629d13ecb70e9d8818278a81c5"
 
   url "https://github.com/Lobobodev/CodexBarMenuBar/releases/download/v#{version}/CodexBarMenuBar-v#{version}.zip",
       verified: "github.com/Lobobodev/CodexBarMenuBar/"
